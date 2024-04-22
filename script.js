dfa3525dc71aa03f87458b40cc7e0021cf8c615e
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-22 14:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
- updateTimer() {
-            var now = new Date();
-            var dateTimeString = now.toLocaleString();
-            document.getElementById('timer').textContent = dateTimeString;
-        }
+let updateTimer = () => {
+    let now = new Date();
+    let dateTimeString = now.toLocaleString();
+    document.getElementById('timer').textContent = dateTimeString;
+};
 
-        updateTimer();
-        setInterval(updateTimer, 1000);
+updateTimer();
+setInterval(updateTimer, 1000);
